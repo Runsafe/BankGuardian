51e9336ebe316244875f969ef4015c3a4f4d43df
Kruithne <kruithne@gmail.com>
2013-09-01 00:04:37 +0100

    Destroy boss, don't despawn it and correctly detect loose entities after a server crash.

--- src/no/runsafe/bankguardian/Encounter.java
+++ src/no/runsafe/bankguardian/Encounter.java
@@ -7,12 +7,17 @@ import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.player.IPlayerTeleport;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
+import no.runsafe.framework.api.event.plugin.IPluginDisabled;
+import no.runsafe.framework.api.event.plugin.IPluginEnabled;
 import no.runsafe.framework.minecraft.RunsafeLocation;
 import no.runsafe.framework.minecraft.RunsafeWorld;
+import no.runsafe.framework.minecraft.entity.RunsafeLivingEntity;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 import org.bukkit.entity.EntityType;
 
-public class Encounter implements IConfigurationChanged, IPlayerTeleport
+import java.util.Iterator;
+
+public class Encounter implements IConfigurationChanged, IPlayerTeleport, IPluginEnabled, IPluginDisabled
 {
 	public Encounter(IOutput output)
 	{
@@ -26,6 +31,29 @@ public class Encounter implements IConfigurationChanged, IPlayerTeleport
 	{
 		spawnPoint = configuration.getConfigValueAsLocation("spawnPoint");
 
+	}
+
+	@Override
+	public void OnPluginDisabled()
+	{
+		if (loaded)
+			this.unloadEncounter();
+	}
+
+	@Override
+	public void OnPluginEnabled()
+	{
+		for (Iterator<NPC> npcIterator = registry.iterator(); npcIterator.hasNext();)
+		{
+			NPC npc = npcIterator.next();
+			if (npc.getFullName().equals("Astalor") && npc.isSpawned())
+			{
+				RunsafeLivingEntity entity = new RunsafeLivingEntity(npc.getBukkitEntity());
+				RunsafeWorld entityWorld = entity.getWorld();
+				if (entityWorld != null && entityWorld.isWorld(spawnPoint.getWorld()))
+					npc.destroy();
+			}
+		}
 	}
 
 	@Override
@@ -51,6 +79,7 @@ public class Encounter implements IConfigurationChanged, IPlayerTeleport
 	private void loadEncounter()
 	{
 		console.fine("Loading encounter");
+		loaded = true; // Set the encounter as loaded.
 		boss = registry.createNPC(EntityType.PLAYER,  "Astalor"); // Create an NPC for the boss.
 		boss.spawn(spawnPoint.getRaw()); // Spawn it in the world at the position stored.
 	}
@@ -58,8 +87,9 @@ public class Encounter implements IConfigurationChanged, IPlayerTeleport
 	private void unloadEncounter()
 	{
 		console.fine("Unloading encounter");
+		loaded = false; // Set the encounter as no longer loaded.
 		if (boss != null) // Check we actually have a boss.
-			boss.despawn(); // Respawn it.
+			boss.destroy(); // Respawn it.
 	}
 
 	private RunsafeLocation spawnPoint;
