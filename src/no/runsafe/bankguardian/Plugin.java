3a5e595c9b66684d29d03f072edae2ab79a6cb52
Kruithne <kruithne@gmail.com>
2013-08-31 04:48:14 +0100

    Initialized new plugin BankGuardian from template

---
+++ src/no/runsafe/bankguardian/Plugin.java
@@ -0,0 +1,12 @@
+package no.runsafe.bankguardian;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Plugin extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+		//addComponent(SomeComponent.class);
+	}
+}
